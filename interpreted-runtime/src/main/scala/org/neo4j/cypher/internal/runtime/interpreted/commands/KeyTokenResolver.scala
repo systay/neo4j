fickludd/neo4j-legacy

Move CommunityPipeBuilder to the interpreted-runtime module

community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/commands/KeyTokenResolver.scala
@@ -17,11 +17,16 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.cypher.internal.compatibility.v3_4.runtime
+package org.neo4j.cypher.internal.runtime.interpreted.commands
 
-import org.neo4j.cypher.internal.frontend.v3_4.semantics.SemanticTable
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
+import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.Expression
+import org.neo4j.cypher.internal.runtime.interpreted.commands.values.KeyToken
 
-case class PipeExecutionBuilderContext(semanticTable: SemanticTable,
-                                       readOnlies: ReadOnlies,
-                                       cardinalities: Cardinalities)
+object KeyTokenResolver {
+  /*this is what you should use!*/
+  def resolveExpressions(expr: Expression, ctx: TokenContext): Expression = expr match {
+    case (keyToken: KeyToken) => keyToken.resolve(ctx)
+    case _                    => expr
+  }
+}

community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/PipeBuilder.scala
@@ -17,16 +17,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan.builders.prepare
+package org.neo4j.cypher.internal.runtime.interpreted.pipes
 
-import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.Expression
-import org.neo4j.cypher.internal.runtime.interpreted.commands.values.KeyToken
-import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
+import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, LogicalPlans}
 
-object KeyTokenResolver {
-  /*this is what you should use!*/
-  def resolveExpressions(expr: Expression, ctx: TokenContext): Expression = expr match {
-    case (keyToken: KeyToken) => keyToken.resolve(ctx)
-    case _                    => expr
-  }
+trait PipeBuilder extends LogicalPlans.Mapper[Pipe] {
+  override def onLeaf(plan: LogicalPlan): Pipe
+  override def onOneChildPlan(plan: LogicalPlan, source: Pipe): Pipe
+  override def onTwoChildPlan(plan: LogicalPlan, lhs: Pipe, rhs: Pipe): Pipe
 }

systay/neo4j

interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/commands/KeyTokenResolver.scala
@@ -17,13 +17,16 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.cypher.internal.compatibility.v3_5.runtime.pipes
+package org.neo4j.cypher.internal.runtime.interpreted.commands
 
-import org.neo4j.cypher.internal.runtime.interpreted.ExecutionContext
-import org.neo4j.cypher.internal.runtime.interpreted.pipes.{Pipe, QueryState}
-import org.neo4j.cypher.internal.util.v3_5.attribution.Id
+import org.neo4j.cypher.internal.planner.v3_5.spi.TokenContext
+import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.Expression
+import org.neo4j.cypher.internal.runtime.interpreted.commands.values.KeyToken
 
-case class DropResultPipe(source: Pipe)(val id: Id = Id.INVALID_ID) extends Pipe {
-
-  protected def internalCreateResults(state: QueryState): Iterator[ExecutionContext] = Iterator.empty
+object KeyTokenResolver {
+  /*this is what you should use!*/
+  def resolveExpressions(expr: Expression, ctx: TokenContext): Expression = expr match {
+    case (keyToken: KeyToken) => keyToken.resolve(ctx)
+    case _                    => expr
+  }
 }

interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/PipeBuilder.scala
@@ -17,11 +17,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.cypher.internal.compatibility.v3_5.runtime
+package org.neo4j.cypher.internal.runtime.interpreted.pipes
 
-import org.neo4j.cypher.internal.frontend.v3_5.semantics.SemanticTable
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, LogicalPlans}
 
-case class PipeExecutionBuilderContext(semanticTable: SemanticTable,
-                                       readOnlies: ReadOnlies,
-                                       cardinalities: Cardinalities)
+trait PipeBuilder extends LogicalPlans.Mapper[Pipe] {
+  override def onLeaf(plan: LogicalPlan): Pipe
+  override def onOneChildPlan(plan: LogicalPlan, source: Pipe): Pipe
+  override def onTwoChildPlan(plan: LogicalPlan, lhs: Pipe, rhs: Pipe): Pipe
+}
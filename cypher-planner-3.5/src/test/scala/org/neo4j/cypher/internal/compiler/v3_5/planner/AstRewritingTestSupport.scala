fickludd/neo4j-legacy

Remove ReadOnlies attribute: only per-query flag is needed.

community/cypher/cypher-planner-3.4/src/test/scala/org/neo4j/cypher/internal/compiler/v3_4/planner/AstRewritingTestSupport.scala
@@ -23,7 +23,7 @@ import org.neo4j.cypher.internal.compiler.v3_4.parser.ParserFixture
 import org.neo4j.cypher.internal.frontend.v3_4.ast.AstConstructionTestSupport
 import org.neo4j.cypher.internal.util.v3_4.test_helpers.CypherTestSupport
 import org.neo4j.cypher.internal.ir.v3_4.PlannerQuery
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies, Solveds}
+import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, Solveds}
 import org.neo4j.cypher.internal.util.v3_4.Cardinality
 import org.neo4j.cypher.internal.util.v3_4.attribution.{Id, SequentialIdGen}
 import scala.language.implicitConversions
@@ -39,16 +39,6 @@ trait LogicalPlanConstructionTestSupport extends CypherTestSupport {
     override def isDefinedAt(id: Id): Boolean = true
 
     override def get(id: Id): PlannerQuery = PlannerQuery.empty
-
-    override def copy(from: Id, to: Id): Unit = {}
-  }
-
-  class StubReadOnlies extends ReadOnlies {
-    override def set(id: Id, t: Boolean): Unit = {}
-
-    override def isDefinedAt(id: Id): Boolean = true
-
-    override def get(id: Id): Boolean = true
 
     override def copy(from: Id, to: Id): Unit = {}
   }

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/BuildInterpretedExecutionPlan.scala
@@ -27,7 +27,7 @@ import org.neo4j.cypher.internal.frontend.v3_4.PlannerName
 import org.neo4j.cypher.internal.frontend.v3_4.phases.CompilationPhaseTracer.CompilationPhase.PIPE_BUILDING
 import org.neo4j.cypher.internal.frontend.v3_4.phases.{InternalNotificationLogger, Phase}
 import org.neo4j.cypher.internal.planner.v3_4.spi.GraphStatistics
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.Cardinalities
 import org.neo4j.cypher.internal.runtime.interpreted.UpdateCountingQueryContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.{CommunityExpressionConverter, ExpressionConverters}
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.PipeExecutionBuilderContext
@@ -46,25 +46,23 @@ object BuildInterpretedExecutionPlan extends Phase[CommunityRuntimeContext, Logi
   override def postConditions = Set(CompilationContains[ExecutionPlan])
 
   override def process(from: LogicalPlanState, context: CommunityRuntimeContext): CompilationState = {
-    val readOnlies = new ReadOnlies
-    from.solveds.mapTo(readOnlies, _.readOnly)
+    val readOnly = from.solveds(from.logicalPlan.id).readOnly
     val cardinalities = from.cardinalities
     val logicalPlan = from.logicalPlan
     val converters = new ExpressionConverters(CommunityExpressionConverter)
     val executionPlanBuilder = new PipeExecutionPlanBuilder(expressionConverters = converters,
                                                             pipeBuilderFactory = InterpretedPipeBuilderFactory)
-    val pipeBuildContext = PipeExecutionBuilderContext(from.semanticTable(), readOnlies, cardinalities)
+    val pipeBuildContext = PipeExecutionBuilderContext(from.semanticTable(), readOnly, cardinalities)
     val pipeInfo = executionPlanBuilder.build(from.periodicCommit, logicalPlan)(pipeBuildContext, context.planContext)
-    val PipeInfo(pipe, updating, periodicCommitInfo) = pipeInfo
+    val PipeInfo(pipe, periodicCommitInfo) = pipeInfo
     val columns = from.statement().returnColumns
-    val resultBuilderFactory = InterpretedExecutionResultBuilderFactory(pipeInfo, columns, logicalPlan)
+    val resultBuilderFactory = InterpretedExecutionResultBuilderFactory(pipe, readOnly, columns, logicalPlan)
     val func = getExecutionPlanFunction(periodicCommitInfo,
-                                        updating,
                                         resultBuilderFactory,
                                         context.notificationLogger,
                                         from.plannerName,
                                         InterpretedRuntimeName,
-                                        readOnlies,
+                                        readOnly,
                                         cardinalities)
 
     val fingerprint = PlanFingerprint.take(context.clock, context.planContext.txIdProvider, context.planContext.statistics)
@@ -78,19 +76,18 @@ object BuildInterpretedExecutionPlan extends Phase[CommunityRuntimeContext, Logi
   }
 
   def getExecutionPlanFunction(periodicCommit: Option[PeriodicCommitInfo],
-                               updating: Boolean,
                                resultBuilderFactory: ExecutionResultBuilderFactory,
                                notificationLogger: InternalNotificationLogger,
                                plannerName: PlannerName,
                                runtimeName: RuntimeName,
-                               readOnlies: ReadOnlies,
+                               readOnly: Boolean,
                                cardinalities: Cardinalities):
   (QueryContext, ExecutionMode, MapValue) => InternalExecutionResult =
     (queryContext: QueryContext, planType: ExecutionMode, params: MapValue) => {
       val builder = resultBuilderFactory.create()
 
       val profiling = planType == ProfileMode
-      val builderContext = if (updating || profiling) new UpdateCountingQueryContext(queryContext) else queryContext
+      val builderContext = if (!readOnly || profiling) new UpdateCountingQueryContext(queryContext) else queryContext
 
       builder.setQueryContext(builderContext)
 
@@ -103,7 +100,7 @@ object BuildInterpretedExecutionPlan extends Phase[CommunityRuntimeContext, Logi
       if (profiling)
         builder.setPipeDecorator(new Profiler(queryContext.transactionalContext.databaseInfo))
 
-      builder.build(planType, params, notificationLogger, plannerName, runtimeName, readOnlies, cardinalities)
+      builder.build(planType, params, notificationLogger, plannerName, runtimeName, readOnly, cardinalities)
     }
 
   /**

community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/PipeExecutionBuilderContext.scala
@@ -20,8 +20,8 @@
 package org.neo4j.cypher.internal.runtime.interpreted.pipes
 
 import org.neo4j.cypher.internal.frontend.v3_4.semantics.SemanticTable
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.Cardinalities
 
 case class PipeExecutionBuilderContext(semanticTable: SemanticTable,
-                                       readOnlies: ReadOnlies,
+                                       readOnly: Boolean,
                                        cardinalities: Cardinalities)

community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/planDescription/LogicalPlan2PlanDescription.scala
@@ -20,7 +20,7 @@
 package org.neo4j.cypher.internal.runtime.planDescription
 
 import org.neo4j.cypher.internal.frontend.v3_4.PlannerName
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.Cardinalities
 import org.neo4j.cypher.internal.runtime.planDescription.InternalPlanDescription.Arguments._
 import org.neo4j.cypher.internal.util.v3_4.InternalException
 import org.neo4j.cypher.internal.v3_4.expressions.{FunctionInvocation, FunctionName, LabelToken, MapExpression, Namespace, PropertyKeyName, PropertyKeyToken, Expression => ASTExpression}
@@ -28,13 +28,12 @@ import org.neo4j.cypher.internal.v3_4.functions.Point
 import org.neo4j.cypher.internal.v3_4.logical.plans
 import org.neo4j.cypher.internal.v3_4.logical.plans._
 
-object LogicalPlan2PlanDescription extends ((LogicalPlan, PlannerName, ReadOnlies, Cardinalities) => InternalPlanDescription) {
+object LogicalPlan2PlanDescription extends ((LogicalPlan, PlannerName, Boolean, Cardinalities) => InternalPlanDescription) {
 
   override def apply(input: LogicalPlan,
                      plannerName: PlannerName,
-                     readOnlies: ReadOnlies,
+                     readOnly: Boolean,
                      cardinalities: Cardinalities): InternalPlanDescription = {
-    val readOnly = readOnlies.get(input.id)
     new LogicalPlan2PlanDescription(readOnly, cardinalities).create(input)
       .addArgument(Version("CYPHER 3.4"))
       .addArgument(RuntimeVersion("3.4"))

community/cypher/runtime-util/src/test/scala/org/neo4j/cypher/internal/runtime/planDescription/LogicalPlan2PlanDescriptionTest.scala
@@ -20,10 +20,10 @@
 package org.neo4j.cypher.internal.runtime.planDescription
 
 import org.neo4j.cypher.internal.planner.v3_4.spi.IDPPlannerName
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.Cardinalities
 import org.neo4j.cypher.internal.runtime.planDescription.InternalPlanDescription.Arguments._
 import org.neo4j.cypher.internal.util.v3_4._
-import org.neo4j.cypher.internal.util.v3_4.attribution.{Id, SequentialIdGen}
+import org.neo4j.cypher.internal.util.v3_4.attribution.{Id, IdGen, SequentialIdGen}
 import org.neo4j.cypher.internal.util.v3_4.test_helpers.CypherFunSuite
 import org.neo4j.cypher.internal.v3_4.expressions.{SemanticDirection, LabelName => AstLabelName, _}
 import org.neo4j.cypher.internal.v3_4.logical.plans._
@@ -32,13 +32,12 @@ import org.scalatest.prop.TableDrivenPropertyChecks
 class LogicalPlan2PlanDescriptionTest extends CypherFunSuite with TableDrivenPropertyChecks {
 
   test("tests") {
-    implicit val idGen = new SequentialIdGen()
-    val readOnlies = new ReadOnlies
+    implicit val idGen: IdGen = new SequentialIdGen()
+    val readOnly = true
     val cardinalities = new Cardinalities
     val id = Id.INVALID_ID
 
     def attach(plan: LogicalPlan, cardinality: Cardinality): LogicalPlan = {
-      readOnlies.set(plan.id, true)
       cardinalities.set(plan.id, cardinality)
       plan
     }
@@ -85,9 +84,9 @@ class LogicalPlan2PlanDescriptionTest extends CypherFunSuite with TableDrivenPro
 
     forAll(modeCombinations) {
       case (logicalPlan: LogicalPlan, expectedPlanDescription: PlanDescriptionImpl) =>
-        val producedPlanDescription = LogicalPlan2PlanDescription(logicalPlan, IDPPlannerName, readOnlies, cardinalities)
+        val producedPlanDescription = LogicalPlan2PlanDescription(logicalPlan, IDPPlannerName, readOnly, cardinalities)
 
-        def shouldBeEqual(a: InternalPlanDescription, b: InternalPlanDescription) = {
+        def shouldBeEqual(a: InternalPlanDescription, b: InternalPlanDescription): Unit = {
           withClue("name")(a.name should equal(b.name))
           withClue("arguments")(a.arguments should equal(b.arguments))
           withClue("variables")(a.variables should equal(b.variables))

systay/neo4j

cypher-planner-3.5/src/test/scala/org/neo4j/cypher/internal/compiler/v3_5/planner/AstRewritingTestSupport.scala
@@ -23,7 +23,7 @@ import org.neo4j.cypher.internal.compiler.v3_5.parser.ParserFixture
 import org.neo4j.cypher.internal.frontend.v3_5.ast.AstConstructionTestSupport
 import org.neo4j.cypher.internal.util.v3_5.test_helpers.CypherTestSupport
 import org.neo4j.cypher.internal.ir.v3_5.PlannerQuery
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies, Solveds}
+import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, Solveds}
 import org.neo4j.cypher.internal.util.v3_5.Cardinality
 import org.neo4j.cypher.internal.util.v3_5.attribution.{Id, SequentialIdGen}
 import scala.language.implicitConversions
@@ -39,16 +39,6 @@ trait LogicalPlanConstructionTestSupport extends CypherTestSupport {
     override def isDefinedAt(id: Id): Boolean = true
 
     override def get(id: Id): PlannerQuery = PlannerQuery.empty
-
-    override def copy(from: Id, to: Id): Unit = {}
-  }
-
-  class StubReadOnlies extends ReadOnlies {
-    override def set(id: Id, t: Boolean): Unit = {}
-
-    override def isDefinedAt(id: Id): Boolean = true
-
-    override def get(id: Id): Boolean = true
 
     override def copy(from: Id, to: Id): Unit = {}
   }

cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/BuildInterpretedExecutionPlan.scala
@@ -27,7 +27,7 @@ import org.neo4j.cypher.internal.frontend.v3_5.PlannerName
 import org.neo4j.cypher.internal.frontend.v3_5.phases.CompilationPhaseTracer.CompilationPhase.PIPE_BUILDING
 import org.neo4j.cypher.internal.frontend.v3_5.phases.{InternalNotificationLogger, Phase}
 import org.neo4j.cypher.internal.planner.v3_5.spi.GraphStatistics
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.Cardinalities
 import org.neo4j.cypher.internal.runtime.interpreted.UpdateCountingQueryContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.{CommunityExpressionConverter, ExpressionConverters}
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.PipeExecutionBuilderContext
@@ -46,25 +46,23 @@ object BuildInterpretedExecutionPlan extends Phase[CommunityRuntimeContext, Logi
   override def postConditions = Set(CompilationContains[ExecutionPlan])
 
   override def process(from: LogicalPlanState, context: CommunityRuntimeContext): CompilationState = {
-    val readOnlies = new ReadOnlies
-    from.solveds.mapTo(readOnlies, _.readOnly)
+    val readOnly = from.solveds(from.logicalPlan.id).readOnly
     val cardinalities = from.cardinalities
     val logicalPlan = from.logicalPlan
     val converters = new ExpressionConverters(CommunityExpressionConverter)
     val executionPlanBuilder = new PipeExecutionPlanBuilder(expressionConverters = converters,
                                                             pipeBuilderFactory = InterpretedPipeBuilderFactory)
-    val pipeBuildContext = PipeExecutionBuilderContext(from.semanticTable(), readOnlies, cardinalities)
+    val pipeBuildContext = PipeExecutionBuilderContext(from.semanticTable(), readOnly, cardinalities)
     val pipeInfo = executionPlanBuilder.build(from.periodicCommit, logicalPlan)(pipeBuildContext, context.planContext)
-    val PipeInfo(pipe, updating, periodicCommitInfo) = pipeInfo
+    val PipeInfo(pipe, periodicCommitInfo) = pipeInfo
     val columns = from.statement().returnColumns
-    val resultBuilderFactory = InterpretedExecutionResultBuilderFactory(pipeInfo, columns, logicalPlan)
+    val resultBuilderFactory = InterpretedExecutionResultBuilderFactory(pipe, readOnly, columns, logicalPlan)
     val func = getExecutionPlanFunction(periodicCommitInfo,
-                                        updating,
                                         resultBuilderFactory,
                                         context.notificationLogger,
                                         from.plannerName,
                                         InterpretedRuntimeName,
-                                        readOnlies,
+                                        readOnly,
                                         cardinalities)
 
     val fingerprint = PlanFingerprint.take(context.clock, context.planContext.txIdProvider, context.planContext.statistics)
@@ -78,19 +76,18 @@ object BuildInterpretedExecutionPlan extends Phase[CommunityRuntimeContext, Logi
   }
 
   def getExecutionPlanFunction(periodicCommit: Option[PeriodicCommitInfo],
-                               updating: Boolean,
                                resultBuilderFactory: ExecutionResultBuilderFactory,
                                notificationLogger: InternalNotificationLogger,
                                plannerName: PlannerName,
                                runtimeName: RuntimeName,
-                               readOnlies: ReadOnlies,
+                               readOnly: Boolean,
                                cardinalities: Cardinalities):
   (QueryContext, ExecutionMode, MapValue) => InternalExecutionResult =
     (queryContext: QueryContext, planType: ExecutionMode, params: MapValue) => {
       val builder = resultBuilderFactory.create()
 
       val profiling = planType == ProfileMode
-      val builderContext = if (updating || profiling) new UpdateCountingQueryContext(queryContext) else queryContext
+      val builderContext = if (!readOnly || profiling) new UpdateCountingQueryContext(queryContext) else queryContext
 
       builder.setQueryContext(builderContext)
 
@@ -103,7 +100,7 @@ object BuildInterpretedExecutionPlan extends Phase[CommunityRuntimeContext, Logi
       if (profiling)
         builder.setPipeDecorator(new Profiler(queryContext.transactionalContext.databaseInfo))
 
-      builder.build(planType, params, notificationLogger, plannerName, runtimeName, readOnlies, cardinalities)
+      builder.build(planType, params, notificationLogger, plannerName, runtimeName, readOnly, cardinalities)
     }
 
   /**

interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/PipeExecutionBuilderContext.scala
@@ -20,8 +20,8 @@
 package org.neo4j.cypher.internal.runtime.interpreted.pipes
 
 import org.neo4j.cypher.internal.frontend.v3_5.semantics.SemanticTable
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.Cardinalities
 
 case class PipeExecutionBuilderContext(semanticTable: SemanticTable,
-                                       readOnlies: ReadOnlies,
+                                       readOnly: Boolean,
                                        cardinalities: Cardinalities)

runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/planDescription/LogicalPlan2PlanDescription.scala
@@ -20,7 +20,7 @@
 package org.neo4j.cypher.internal.runtime.planDescription
 
 import org.neo4j.cypher.internal.frontend.v3_5.PlannerName
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.Cardinalities
 import org.neo4j.cypher.internal.runtime.planDescription.InternalPlanDescription.Arguments._
 import org.neo4j.cypher.internal.util.v3_5.InternalException
 import org.neo4j.cypher.internal.v3_5.expressions.{FunctionInvocation, FunctionName, LabelToken, MapExpression, Namespace, PropertyKeyName, PropertyKeyToken, Expression => ASTExpression}
@@ -28,13 +28,12 @@ import org.neo4j.cypher.internal.v3_5.functions.Point
 import org.neo4j.cypher.internal.v3_5.logical.plans
 import org.neo4j.cypher.internal.v3_5.logical.plans._
 
-object LogicalPlan2PlanDescription extends ((LogicalPlan, PlannerName, ReadOnlies, Cardinalities) => InternalPlanDescription) {
+object LogicalPlan2PlanDescription extends ((LogicalPlan, PlannerName, Boolean, Cardinalities) => InternalPlanDescription) {
 
   override def apply(input: LogicalPlan,
                      plannerName: PlannerName,
-                     readOnlies: ReadOnlies,
+                     readOnly: Boolean,
                      cardinalities: Cardinalities): InternalPlanDescription = {
-    val readOnly = readOnlies.get(input.id)
     new LogicalPlan2PlanDescription(readOnly, cardinalities).create(input)
       .addArgument(Version("CYPHER "+plannerName.version))
       .addArgument(RuntimeVersion("3.4"))

runtime-util/src/test/scala/org/neo4j/cypher/internal/runtime/planDescription/LogicalPlan2PlanDescriptionTest.scala
@@ -20,10 +20,10 @@
 package org.neo4j.cypher.internal.runtime.planDescription
 
 import org.neo4j.cypher.internal.planner.v3_5.spi.IDPPlannerName
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
+import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.Cardinalities
 import org.neo4j.cypher.internal.runtime.planDescription.InternalPlanDescription.Arguments._
 import org.neo4j.cypher.internal.util.v3_5._
-import org.neo4j.cypher.internal.util.v3_5.attribution.{Id, SequentialIdGen}
+import org.neo4j.cypher.internal.util.v3_5.attribution.{Id, IdGen, SequentialIdGen}
 import org.neo4j.cypher.internal.util.v3_5.test_helpers.CypherFunSuite
 import org.neo4j.cypher.internal.v3_5.expressions.{SemanticDirection, LabelName => AstLabelName, _}
 import org.neo4j.cypher.internal.v3_5.logical.plans._
@@ -32,13 +32,12 @@ import org.scalatest.prop.TableDrivenPropertyChecks
 class LogicalPlan2PlanDescriptionTest extends CypherFunSuite with TableDrivenPropertyChecks {
 
   test("tests") {
-    implicit val idGen = new SequentialIdGen()
-    val readOnlies = new ReadOnlies
+    implicit val idGen: IdGen = new SequentialIdGen()
+    val readOnly = true
     val cardinalities = new Cardinalities
     val id = Id.INVALID_ID
 
     def attach(plan: LogicalPlan, cardinality: Cardinality): LogicalPlan = {
-      readOnlies.set(plan.id, true)
       cardinalities.set(plan.id, cardinality)
       plan
     }
@@ -85,9 +84,9 @@ class LogicalPlan2PlanDescriptionTest extends CypherFunSuite with TableDrivenPro
 
     forAll(modeCombinations) {
       case (logicalPlan: LogicalPlan, expectedPlanDescription: PlanDescriptionImpl) =>
-        val producedPlanDescription = LogicalPlan2PlanDescription(logicalPlan, IDPPlannerName, readOnlies, cardinalities)
+        val producedPlanDescription = LogicalPlan2PlanDescription(logicalPlan, IDPPlannerName, readOnly, cardinalities)
 
-        def shouldBeEqual(a: InternalPlanDescription, b: InternalPlanDescription) = {
+        def shouldBeEqual(a: InternalPlanDescription, b: InternalPlanDescription): Unit = {
           withClue("name")(a.name should equal(b.name))
           withClue("arguments")(a.arguments should equal(b.arguments))
           withClue("variables")(a.variables should equal(b.variables))
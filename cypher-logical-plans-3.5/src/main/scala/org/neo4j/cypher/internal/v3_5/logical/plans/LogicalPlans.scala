fickludd/neo4j-legacy

Generalize mapping of logical plans.

community/cypher/cypher-logical-plans-3.4/src/main/scala/org/neo4j/cypher/internal/v3_4/logical/plans/LogicalPlans.scala
@@ -1,0 +1,110 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.v3_4.logical.plans
+
+import scala.collection.mutable
+
+object LogicalPlans {
+
+  trait Mapper[T] {
+    def onLeaf(plan: LogicalPlan): T
+    def onOneChildPlan(plan: LogicalPlan, source: T): T
+    def onTwoChildPlan(plan: LogicalPlan, lhs: T, rhs: T): T
+  }
+
+  /**
+    * Traverses the logical plan tree structure and maps the tree in a bottom but fashion.
+    *
+    * Given a logical plan such as:
+    *
+    *         a
+    *        / \
+    *       b   c
+    *      /   / \
+    *     d   e   f
+    *
+    * the mapper will be called in the following sequence:
+    *
+    *   F = mapLeaf(f)
+    *   E = mapLeaf(e)
+    *   C = mapTwoChildPlan(c, E, F)
+    *   D = mapLeaf(d)
+    *   B = mapOneChingPlan(b, D)
+    *   A = mapTwoChildPlan(a, B, C)
+    */
+  def map[T](plan: LogicalPlan, mapper: Mapper[T]): T = {
+    val planStack = new mutable.Stack[LogicalPlan]()
+    val resultStack = new mutable.Stack[T]()
+    var comingFrom = plan
+    def populate(plan: LogicalPlan) = {
+      var current = plan
+      while (!current.isLeaf) {
+        planStack.push(current)
+        (current.lhs, current.rhs) match {
+          case (Some(_), Some(right)) =>
+            current = right
+
+          case (Some(left), None) =>
+            current = left
+          case _ => throw new IllegalStateException("This must not be!")
+        }
+      }
+      comingFrom = current
+      planStack.push(current)
+    }
+
+    populate(plan)
+
+    while (planStack.nonEmpty) {
+      val current = planStack.pop()
+
+      (current.lhs, current.rhs) match {
+        case (None, None) =>
+          val result = mapper.onLeaf(current)
+          resultStack.push(result)
+
+        case (Some(_), None) =>
+          val source = resultStack.pop()
+          val result = mapper.onOneChildPlan(current, source)
+          resultStack.push(result)
+
+        case (Some(left), Some(right)) if right == left =>
+          throw new IllegalStateException(s"Tried to map bad logical plan. LHS and RHS must never be the same: op: $current\nfull plan: $plan")
+
+        case (Some(left), Some(_)) if comingFrom == left =>
+          val arg1 = resultStack.pop()
+          val arg2 = resultStack.pop()
+          val result = mapper.onTwoChildPlan(current, arg1, arg2)
+          resultStack.push(result)
+
+        case (Some(left), Some(right)) if comingFrom == right =>
+          planStack.push(current)
+          populate(left)
+      }
+
+      comingFrom = current
+
+    }
+
+    val result = resultStack.pop()
+    assert(resultStack.isEmpty, "Should have emptied the stack of pipes by now!")
+    result
+  }
+}

community/cypher/cypher-logical-plans-3.4/src/test/scala/org/neo4j/cypher/internal/v3_4/logical/plans/LogicalPlansTest.scala
@@ -1,0 +1,88 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.v3_4.logical.plans
+
+import org.neo4j.cypher.internal.util.v3_4.InputPosition
+import org.neo4j.cypher.internal.util.v3_4.attribution.{IdGen, SequentialIdGen}
+import org.neo4j.cypher.internal.util.v3_4.test_helpers.CypherFunSuite
+import org.neo4j.cypher.internal.v3_4.expressions.True
+
+import scala.collection.mutable.ArrayBuffer
+
+class LogicalPlansTest extends CypherFunSuite {
+
+  val pos = InputPosition(1,1,1)
+
+  test("LogicalPlans.map") {
+
+    implicit val idGen: IdGen = new SequentialIdGen
+
+   /*
+    *         a
+    *        / \
+    *       b   c
+    *      /   / \
+    *     d   e   f
+    */
+    val d = AllNodesScan("d", Set.empty)
+    val e = AllNodesScan("e", Set.empty)
+    val f = AllNodesScan("f", Set.empty)
+    val b = Selection(List(True()(pos)), d)
+    val c = Apply(e, f)
+    val a = Apply(b, c)
+
+    val idStrings = new IdStrings()
+    LogicalPlans.map(a, idStrings)
+
+    idStrings.calls should equal(List(
+      "onLeaf Id(2)",
+      "onLeaf Id(1)",
+      "onTwoChildPlan Id(4) Id(1) Id(2)",
+      "onLeaf Id(0)",
+      "onOneChildPlan Id(3) Id(0)",
+      "onTwoChildPlan Id(5) Id(3) Id(4)"
+    ))
+  }
+
+  class IdStrings() extends LogicalPlans.Mapper[String] {
+
+    val calls = new ArrayBuffer[String]
+
+    override def onLeaf(plan: LogicalPlan): String = {
+      val str = id(plan)
+      calls += s"onLeaf $str"
+      str
+    }
+
+    override def onOneChildPlan(plan: LogicalPlan, source: String): String = {
+      val str = id(plan)
+      calls += s"onOneChildPlan $str $source"
+      str
+    }
+
+    override def onTwoChildPlan(plan: LogicalPlan, lhs: String, rhs: String): String = {
+      val str = id(plan)
+      calls += s"onTwoChildPlan $str $lhs $rhs"
+      str
+    }
+  }
+
+  private def id(plan: LogicalPlan) = "Id("+plan.id.x+")"
+}

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/PipeExecutionPlanBuilder.scala
@@ -24,10 +24,7 @@ import org.neo4j.cypher.internal.ir.v3_4.PeriodicCommit
 import org.neo4j.cypher.internal.planner.v3_4.spi.PlanContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.Pipe
-import org.neo4j.cypher.internal.util.v3_4.InternalException
-import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
-
-import scala.collection.mutable
+import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, LogicalPlans, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
 
 class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
@@ -40,93 +37,11 @@ class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
     PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo, context.plannerName)
   }
 
-  /*
-  Traverses the logical plan tree structure and builds up the corresponding pipe structure. Given a logical plan such as:
-
-          a
-         / \
-        b   c
-       /   / \
-      d   e   f
-
-   populate(a) starts the session, and eagerly adds [a, c, f] to the plan stack. We then immediately pop 'f' from the
-   plan stack, we build a pipe for it add it to the pipe stack, and pop 'c' from the plan stack. Since we are coming from
-   'f', we add [c, e] to the stack and then pop 'e' out again. This is a leaf, so we build a pipe for it and add it to the
-   pipe stack. We now pop 'c' from the plan stack again. This time we are coming from 'e', and so we know we can use
-   two pipes from the pipe stack to use when building 'c'. We add this pipe to the pipe stack and pop 'a' from the plan
-   stack. Since we are coming from 'a's RHS, we add [a,b,d] to the stack. Next step is to pop 'd' out, and build a pipe
-   for it, storing it in the pipe stack. Pop ut 'b' from the plan stack, one pipe from the pipe stack, and build a pipe for 'b'.
-   Next we pop out 'a', and this time we are coming from the LHS, and we can now pop two pipes from the pipe stack to
-   build the pipe for 'a'. Thanks for reading this far - I didn't think we would make it!
-   */
   private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Pipe = {
     val pipeBuilder = pipeBuilderFactory(recurse = p => buildPipe(p),
                                          readOnly = context.readOnlies.get(plan.id),
                                          expressionConverters = expressionConverters)
-
-    val planStack = new mutable.Stack[LogicalPlan]()
-    val pipeStack = new mutable.Stack[Pipe]()
-    var comingFrom = plan
-    def populate(plan: LogicalPlan) = {
-      var current = plan
-      while (!current.isLeaf) {
-        planStack.push(current)
-        (current.lhs, current.rhs) match {
-          case (Some(_), Some(right)) =>
-            current = right
-
-          case (Some(left), None) =>
-            current = left
-          case _ => throw new InternalException("This must not be!")
-        }
-      }
-      comingFrom = current
-      planStack.push(current)
-    }
-
-    populate(plan)
-
-    while (planStack.nonEmpty) {
-      val current = planStack.pop()
-
-      (current.lhs, current.rhs) match {
-        case (None, None) =>
-          val newPipe = pipeBuilder
-            .build(current)
-
-          pipeStack.push(newPipe)
-
-        case (Some(_), None) =>
-          val source = pipeStack.pop()
-          val newPipe = pipeBuilder
-            .build(current, source)
-
-          pipeStack.push(newPipe)
-
-        case (Some(left), Some(right)) if right == left =>
-          throw new InternalException(s"Tried to build pipes from bad logical plan. LHS and RHS must never be the same: op: $current\nfull plan: $plan")
-
-        case (Some(left), Some(_)) if comingFrom == left =>
-          val arg1 = pipeStack.pop()
-          val arg2 = pipeStack.pop()
-          val newPipe = pipeBuilder
-            .build(current, arg1, arg2)
-
-          pipeStack.push(newPipe)
-
-        case (Some(left), Some(right)) if comingFrom == right =>
-          planStack.push(current)
-          populate(left)
-      }
-
-      comingFrom = current
-
-    }
-
-    val result = pipeStack.pop()
-    assert(pipeStack.isEmpty, "Should have emptied the stack of pipes by now!")
-
-    result
+    LogicalPlans.map(plan, pipeBuilder)
   }
 }
 
@@ -137,11 +52,10 @@ object CommunityPipeBuilderFactory extends PipeBuilderFactory {
            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): CommunityPipeBuilder = {
     CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
   }
-
 }
 
-trait PipeBuilder {
-  def build(plan: LogicalPlan): Pipe
-  def build(plan: LogicalPlan, source: Pipe): Pipe
-  def build(plan: LogicalPlan, lhs: Pipe, rhs: Pipe): Pipe
+trait PipeBuilder extends LogicalPlans.Mapper[Pipe] {
+  override def onLeaf(plan: LogicalPlan): Pipe
+  override def onOneChildPlan(plan: LogicalPlan, source: Pipe): Pipe
+  override def onTwoChildPlan(plan: LogicalPlan, lhs: Pipe, rhs: Pipe): Pipe
 }

systay/neo4j

cypher-logical-plans-3.5/src/main/scala/org/neo4j/cypher/internal/v3_5/logical/plans/LogicalPlans.scala
@@ -1,0 +1,110 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.v3_5.logical.plans
+
+import scala.collection.mutable
+
+object LogicalPlans {
+
+  trait Mapper[T] {
+    def onLeaf(plan: LogicalPlan): T
+    def onOneChildPlan(plan: LogicalPlan, source: T): T
+    def onTwoChildPlan(plan: LogicalPlan, lhs: T, rhs: T): T
+  }
+
+  /**
+    * Traverses the logical plan tree structure and maps the tree in a bottom but fashion.
+    *
+    * Given a logical plan such as:
+    *
+    *         a
+    *        / \
+    *       b   c
+    *      /   / \
+    *     d   e   f
+    *
+    * the mapper will be called in the following sequence:
+    *
+    *   F = mapLeaf(f)
+    *   E = mapLeaf(e)
+    *   C = mapTwoChildPlan(c, E, F)
+    *   D = mapLeaf(d)
+    *   B = mapOneChingPlan(b, D)
+    *   A = mapTwoChildPlan(a, B, C)
+    */
+  def map[T](plan: LogicalPlan, mapper: Mapper[T]): T = {
+    val planStack = new mutable.Stack[LogicalPlan]()
+    val resultStack = new mutable.Stack[T]()
+    var comingFrom = plan
+    def populate(plan: LogicalPlan) = {
+      var current = plan
+      while (!current.isLeaf) {
+        planStack.push(current)
+        (current.lhs, current.rhs) match {
+          case (Some(_), Some(right)) =>
+            current = right
+
+          case (Some(left), None) =>
+            current = left
+          case _ => throw new IllegalStateException("This must not be!")
+        }
+      }
+      comingFrom = current
+      planStack.push(current)
+    }
+
+    populate(plan)
+
+    while (planStack.nonEmpty) {
+      val current = planStack.pop()
+
+      (current.lhs, current.rhs) match {
+        case (None, None) =>
+          val result = mapper.onLeaf(current)
+          resultStack.push(result)
+
+        case (Some(_), None) =>
+          val source = resultStack.pop()
+          val result = mapper.onOneChildPlan(current, source)
+          resultStack.push(result)
+
+        case (Some(left), Some(right)) if right == left =>
+          throw new IllegalStateException(s"Tried to map bad logical plan. LHS and RHS must never be the same: op: $current\nfull plan: $plan")
+
+        case (Some(left), Some(_)) if comingFrom == left =>
+          val arg1 = resultStack.pop()
+          val arg2 = resultStack.pop()
+          val result = mapper.onTwoChildPlan(current, arg1, arg2)
+          resultStack.push(result)
+
+        case (Some(left), Some(right)) if comingFrom == right =>
+          planStack.push(current)
+          populate(left)
+      }
+
+      comingFrom = current
+
+    }
+
+    val result = resultStack.pop()
+    assert(resultStack.isEmpty, "Should have emptied the stack of pipes by now!")
+    result
+  }
+}

cypher-logical-plans-3.5/src/test/scala/org/neo4j/cypher/internal/v3_5/logical/plans/LogicalPlansTest.scala
@@ -1,0 +1,88 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.v3_5.logical.plans
+
+import org.neo4j.cypher.internal.util.v3_5.InputPosition
+import org.neo4j.cypher.internal.util.v3_5.attribution.{IdGen, SequentialIdGen}
+import org.neo4j.cypher.internal.util.v3_5.test_helpers.CypherFunSuite
+import org.neo4j.cypher.internal.v3_5.expressions.True
+
+import scala.collection.mutable.ArrayBuffer
+
+class LogicalPlansTest extends CypherFunSuite {
+
+  val pos = InputPosition(1,1,1)
+
+  test("LogicalPlans.map") {
+
+    implicit val idGen: IdGen = new SequentialIdGen
+
+   /*
+    *         a
+    *        / \
+    *       b   c
+    *      /   / \
+    *     d   e   f
+    */
+    val d = AllNodesScan("d", Set.empty)
+    val e = AllNodesScan("e", Set.empty)
+    val f = AllNodesScan("f", Set.empty)
+    val b = Selection(List(True()(pos)), d)
+    val c = Apply(e, f)
+    val a = Apply(b, c)
+
+    val idStrings = new IdStrings()
+    LogicalPlans.map(a, idStrings)
+
+    idStrings.calls should equal(List(
+      "onLeaf Id(2)",
+      "onLeaf Id(1)",
+      "onTwoChildPlan Id(4) Id(1) Id(2)",
+      "onLeaf Id(0)",
+      "onOneChildPlan Id(3) Id(0)",
+      "onTwoChildPlan Id(5) Id(3) Id(4)"
+    ))
+  }
+
+  class IdStrings() extends LogicalPlans.Mapper[String] {
+
+    val calls = new ArrayBuffer[String]
+
+    override def onLeaf(plan: LogicalPlan): String = {
+      val str = id(plan)
+      calls += s"onLeaf $str"
+      str
+    }
+
+    override def onOneChildPlan(plan: LogicalPlan, source: String): String = {
+      val str = id(plan)
+      calls += s"onOneChildPlan $str $source"
+      str
+    }
+
+    override def onTwoChildPlan(plan: LogicalPlan, lhs: String, rhs: String): String = {
+      val str = id(plan)
+      calls += s"onTwoChildPlan $str $lhs $rhs"
+      str
+    }
+  }
+
+  private def id(plan: LogicalPlan) = "Id("+plan.id.x+")"
+}

cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/PipeExecutionPlanBuilder.scala
@@ -24,10 +24,7 @@ import org.neo4j.cypher.internal.ir.v3_5.PeriodicCommit
 import org.neo4j.cypher.internal.planner.v3_5.spi.PlanContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.Pipe
-import org.neo4j.cypher.internal.util.v3_5.InternalException
-import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
-
-import scala.collection.mutable
+import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, LogicalPlans, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
 
 class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
@@ -40,93 +37,11 @@ class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
     PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo, context.plannerName)
   }
 
-  /*
-  Traverses the logical plan tree structure and builds up the corresponding pipe structure. Given a logical plan such as:
-
-          a
-         / \
-        b   c
-       /   / \
-      d   e   f
-
-   populate(a) starts the session, and eagerly adds [a, c, f] to the plan stack. We then immediately pop 'f' from the
-   plan stack, we build a pipe for it add it to the pipe stack, and pop 'c' from the plan stack. Since we are coming from
-   'f', we add [c, e] to the stack and then pop 'e' out again. This is a leaf, so we build a pipe for it and add it to the
-   pipe stack. We now pop 'c' from the plan stack again. This time we are coming from 'e', and so we know we can use
-   two pipes from the pipe stack to use when building 'c'. We add this pipe to the pipe stack and pop 'a' from the plan
-   stack. Since we are coming from 'a's RHS, we add [a,b,d] to the stack. Next step is to pop 'd' out, and build a pipe
-   for it, storing it in the pipe stack. Pop ut 'b' from the plan stack, one pipe from the pipe stack, and build a pipe for 'b'.
-   Next we pop out 'a', and this time we are coming from the LHS, and we can now pop two pipes from the pipe stack to
-   build the pipe for 'a'. Thanks for reading this far - I didn't think we would make it!
-   */
   private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Pipe = {
     val pipeBuilder = pipeBuilderFactory(recurse = p => buildPipe(p),
                                          readOnly = context.readOnlies.get(plan.id),
                                          expressionConverters = expressionConverters)
-
-    val planStack = new mutable.Stack[LogicalPlan]()
-    val pipeStack = new mutable.Stack[Pipe]()
-    var comingFrom = plan
-    def populate(plan: LogicalPlan) = {
-      var current = plan
-      while (!current.isLeaf) {
-        planStack.push(current)
-        (current.lhs, current.rhs) match {
-          case (Some(_), Some(right)) =>
-            current = right
-
-          case (Some(left), None) =>
-            current = left
-          case _ => throw new InternalException("This must not be!")
-        }
-      }
-      comingFrom = current
-      planStack.push(current)
-    }
-
-    populate(plan)
-
-    while (planStack.nonEmpty) {
-      val current = planStack.pop()
-
-      (current.lhs, current.rhs) match {
-        case (None, None) =>
-          val newPipe = pipeBuilder
-            .build(current)
-
-          pipeStack.push(newPipe)
-
-        case (Some(_), None) =>
-          val source = pipeStack.pop()
-          val newPipe = pipeBuilder
-            .build(current, source)
-
-          pipeStack.push(newPipe)
-
-        case (Some(left), Some(right)) if right == left =>
-          throw new InternalException(s"Tried to build pipes from bad logical plan. LHS and RHS must never be the same: op: $current\nfull plan: $plan")
-
-        case (Some(left), Some(_)) if comingFrom == left =>
-          val arg1 = pipeStack.pop()
-          val arg2 = pipeStack.pop()
-          val newPipe = pipeBuilder
-            .build(current, arg1, arg2)
-
-          pipeStack.push(newPipe)
-
-        case (Some(left), Some(right)) if comingFrom == right =>
-          planStack.push(current)
-          populate(left)
-      }
-
-      comingFrom = current
-
-    }
-
-    val result = pipeStack.pop()
-    assert(pipeStack.isEmpty, "Should have emptied the stack of pipes by now!")
-
-    result
+    LogicalPlans.map(plan, pipeBuilder)
   }
 }
 
@@ -137,11 +52,10 @@ object CommunityPipeBuilderFactory extends PipeBuilderFactory {
            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): CommunityPipeBuilder = {
     CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
   }
-
 }
 
-trait PipeBuilder {
-  def build(plan: LogicalPlan): Pipe
-  def build(plan: LogicalPlan, source: Pipe): Pipe
-  def build(plan: LogicalPlan, lhs: Pipe, rhs: Pipe): Pipe
+trait PipeBuilder extends LogicalPlans.Mapper[Pipe] {
+  override def onLeaf(plan: LogicalPlan): Pipe
+  override def onOneChildPlan(plan: LogicalPlan, source: Pipe): Pipe
+  override def onTwoChildPlan(plan: LogicalPlan, lhs: Pipe, rhs: Pipe): Pipe
 }